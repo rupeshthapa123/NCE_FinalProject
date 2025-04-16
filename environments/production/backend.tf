f6143b10ae3070d3a417e2557c7e73f01f079873
Rupesh Thapa <rupesh.thapa2050@gmail.com>
2025-04-16 03:26:39 -0400

    Update backend.tf

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,13 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "acs-project-prod-2025"
-#    key            = "terraform.tfstate"
-#    region         = "us-east-1"
-#  }
-#}
-
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "acs-project-prod-2025"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
   }
 }
