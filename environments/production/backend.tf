fa7932f626f6f6a70a0baffeaabec5bb36aed9d0
Rupesh Thapa <rupesh.thapa2050@gmail.com>
2025-04-16 03:29:21 -0400

    Update s3 bucket

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "acs-project-prod"
+    bucket         = "acs-project-final-prod"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
