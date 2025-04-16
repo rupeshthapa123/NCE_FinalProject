33b76d61f7e5a8fca6182db69ba9300eda694c5e
Rupesh Thapa <rupesh.thapa2050@gmail.com>
2025-04-16 03:30:38 -0400

    Update s3

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "acs-project-staging-s3"
+    bucket         = "acs-project-staging"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
